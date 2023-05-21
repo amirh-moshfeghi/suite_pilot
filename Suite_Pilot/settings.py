81f862144fc6fa2da00b048e930e4c75a1301f27
unknown <amirh.moshfeghi@gmail.com>
2023-05-21 21:21:48 +0330

    asset changed/bug fixed

--- Suite_Pilot/settings.py
+++ Suite_Pilot/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
